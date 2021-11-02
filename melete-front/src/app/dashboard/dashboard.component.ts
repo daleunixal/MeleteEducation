4a8c353533ab5f7c1530b3937e00284926561006
Nikita Zerniukov <n.zernyukov@artsofte.ru>
2021-11-02 19:11:21 +0500

    Some improve

--- melete-front/src/app/dashboard/dashboard.component.ts
+++ melete-front/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 @Component({
     selector: "medu-dashboard",
     templateUrl: "dashboard.component.html",
+    styleUrls: ["/styles/style.less"],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent implements OnInit{
